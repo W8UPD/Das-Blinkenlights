f8e2b349ffc1337be6cb88e45aeb713b2c08bffe
Chris Egeland <chris@chrisegeland.com>
2011-06-20 21:12:39 -0400

    Enmorse.

--- morse_convert.c
+++ morse_convert.c
@@ -111,7 +111,7 @@ int main(int argc, char* argv[]) {
     perform_blink(argv[1]);
     return 0;
   } else {
-    printf("Usage: ./morse 'text to morse'\n");
+    printf("Usage: ./morse 'text to enmorse'\n");
     return 1;
   }
 }
